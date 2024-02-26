caaebccc806b0a2c74e7f58c9edb769710fc0ab3
akashvtrs <alexakash547@gmail.com>
2024-02-26 06:19:38 +0530

    details screen customization

--- lib/business_profile/details.dart
+++ lib/business_profile/details.dart
@@ -31,6 +31,58 @@ class _DetailsState extends State<Details> {
   TextEditingController pincode_controller = TextEditingController();
   TextEditingController business_description_controller =
       TextEditingController();
+      int _selectedIndex = 0; 
+      void _onItemTapped(int index) {
+  setState(() {
+    _selectedIndex = index;
+    if (index == 1) {
+      setState(() {
+        clear();
+      });
+    }
+    if (index==0) {
+      setState(() {
+        save();
+      });
+      
+    }
+    if (index==2) {
+      setState(() {
+        save_pass();
+      });
+      
+      
+    }
+   
+  });
+}
+void save_pass() {
+                      if (
+                        business_name_controller.text.isNotEmpty && owner_name_controller.text.isNotEmpty 
+            && phone_no_controller.text.isNotEmpty && email_controller.text.isNotEmpty && address_controller.text.isNotEmpty && pincode_controller.text.isNotEmpty
+            && business_description_controller.text.isNotEmpty
+                      ) {
+                         Business_Name = business_name_controller.text;
+            Owner_Name = owner_name_controller.text;
+            Phone_No = phone_no_controller.text;
+            Email = email_controller.text;
+            Address = address_controller.text;
+            Pincode = pincode_controller.text;
+            Business_Description = business_description_controller.text;
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                Identities(name_owner:Owner_Name, name_Business: Business_Name,
+      name_customername:[],
+      no_phoneno:[],
+      items_noofitems:[],
+      amount_totalamount:[],
+      bal_balance:[]),
+                          ));
+                      }
+                     
+                    }
 
   void save() {
     setState(() {
@@ -75,15 +127,17 @@ business_description_controller.text="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
-        title: Text("Details"),
+        backgroundColor: Colors.blue.shade100,
+        title: Center(child: Text("BUSINESS PROFILE", style: TextStyle(fontSize: 20)),),
       ),
       body: Padding(
         padding: EdgeInsets.only(left: 10.0, right: 10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text("Profile Section"),
+            
             TextField(
               controller: business_name_controller,
               decoration: InputDecoration(
@@ -133,56 +187,33 @@ business_description_controller.text="";
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(8.0)))),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton(
-                    style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue),
-                    onPressed: save,
-                    child: Text("save")),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue),
-                    onPressed: clear,
-                    child: Text("clear")),
-                IconButton(
-                    onPressed: () {
-                      if (
-                        business_name_controller.text.isNotEmpty && owner_name_controller.text.isNotEmpty 
-            && phone_no_controller.text.isNotEmpty && email_controller.text.isNotEmpty && address_controller.text.isNotEmpty && pincode_controller.text.isNotEmpty
-            && business_description_controller.text.isNotEmpty
-                      ) {
-                         Business_Name = business_name_controller.text;
-            Owner_Name = owner_name_controller.text;
-            Phone_No = phone_no_controller.text;
-            Email = email_controller.text;
-            Address = address_controller.text;
-            Pincode = pincode_controller.text;
-            Business_Description = business_description_controller.text;
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                Identities(name_owner:Owner_Name, name_Business: Business_Name,
-      name_customername:[],
-      no_phoneno:[],
-      items_noofitems:[],
-      amount_totalamount:[],
-      bal_balance:[]),
-                          ));
-                      }
-                     
-                    },
-                    icon: Icon(Icons.send)),
-              ],
-            ),
-            
+           
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex, //New
+  onTap:_onItemTapped, 
+    items: const <BottomNavigationBarItem>[
+      
+      
+      BottomNavigationBarItem(
+        icon: Icon(Icons.save),
+        label: 'Save',
+        
+      ),
+      BottomNavigationBarItem(
+
+        icon: Icon(Icons.delete),
+        label: 'clear',
+        
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.send),
+        label: 'Next',
+      ),
+    ],
+  ),
     );
   }
 }
