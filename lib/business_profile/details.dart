167c32fae7df8b90d226ec5c3337bf3b13c012ac
akashvtrs <alexakash547@gmail.com>
2024-02-26 06:01:44 +0530

    details screen

--- lib/business_profile/details.dart
+++ lib/business_profile/details.dart
@@ -34,13 +34,19 @@ class _DetailsState extends State<Details> {
 
   void save() {
     setState(() {
-      Business_Name = business_name_controller.text;
+      if (business_name_controller.text.isNotEmpty && owner_name_controller.text.isNotEmpty 
+      && phone_no_controller.text.isNotEmpty && email_controller.text.isNotEmpty && address_controller.text.isNotEmpty && pincode_controller.text.isNotEmpty
+      && business_description_controller.text.isNotEmpty
+      ) {
+        Business_Name = business_name_controller.text;
       Owner_Name = owner_name_controller.text;
       Phone_No = phone_no_controller.text;
       Email = email_controller.text;
       Address = address_controller.text;
       Pincode = pincode_controller.text;
       Business_Description = business_description_controller.text;
+      }
+      
     });
   }
 
@@ -53,6 +59,15 @@ class _DetailsState extends State<Details> {
       Address = "";
       Pincode = "";
       Business_Description = "";
+business_name_controller.text="";
+owner_name_controller.text="";
+phone_no_controller.text="";
+email_controller.text="";
+address_controller.text="";
+pincode_controller.text="";
+business_description_controller.text="";
+
+
     });
   }
  
@@ -63,110 +78,109 @@ class _DetailsState extends State<Details> {
       appBar: AppBar(
         title: Text("Details"),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(left: 10.0, right: 10.0),
-          child: Column(
-            children: [
-              Text("Profile Section"),
-              TextField(
-                controller: business_name_controller,
-                decoration: InputDecoration(
-                    label: Text("Business Name"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: owner_name_controller,
-                decoration: InputDecoration(
-                    label: Text("Owner Name"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: phone_no_controller,
-                decoration: InputDecoration(
-                    label: Text("Phone No"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: email_controller,
-                decoration: InputDecoration(
-                    label: Text("Email"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: address_controller,
-                decoration: InputDecoration(
-                    label: Text("Address"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: pincode_controller,
-                decoration: InputDecoration(
-                    label: Text("Pincode"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              TextField(
-                controller: business_description_controller,
-                decoration: InputDecoration(
-                    label: Text("Business Description"),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)))),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.blue),
-                      onPressed: save,
-                      child: Text("save")),
-                  TextButton(
-                      style: TextButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.blue),
-                      onPressed: clear,
-                      child: Text("clear")),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  Identities(name_owner:Owner_Name, name_Business: Business_Name,
-name_customername:[],
-no_phoneno:[],
-items_noofitems:[],
-amount_totalamount:[],
-bal_balance:[]),
-                            ));
-                      },
-                      icon: Icon(Icons.send)),
-                ],
-              ),
-              Text(Business_Name),
-              Text(Owner_Name),
-              Text(Phone_No),
-              Text(Email),
-              Text(Address),
-              Text(Pincode),
-              Text(Business_Description),
-              Text("-----------------------------------------"),
-              Text(Business_Name),
-              Text(Owner_Name),
-              Text(Phone_No),
-              Text(Email),
-              Text(Address),
-              Text(Pincode),
-              Text(Business_Description),
-            ],
-          ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Profile Section"),
+            TextField(
+              controller: business_name_controller,
+              decoration: InputDecoration(
+                  label: Text("Business Name"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: owner_name_controller,
+              decoration: InputDecoration(
+                  label: Text("Owner Name"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: phone_no_controller,
+              decoration: InputDecoration(
+                  label: Text("Phone No"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: email_controller,
+              decoration: InputDecoration(
+                  label: Text("Email"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: address_controller,
+              decoration: InputDecoration(
+                  label: Text("Address"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: pincode_controller,
+              decoration: InputDecoration(
+                  label: Text("Pincode"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            TextField(
+              controller: business_description_controller,
+              decoration: InputDecoration(
+                  label: Text("Business Description"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)))),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue),
+                    onPressed: save,
+                    child: Text("save")),
+                TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue),
+                    onPressed: clear,
+                    child: Text("clear")),
+                IconButton(
+                    onPressed: () {
+                      if (
+                        business_name_controller.text.isNotEmpty && owner_name_controller.text.isNotEmpty 
+            && phone_no_controller.text.isNotEmpty && email_controller.text.isNotEmpty && address_controller.text.isNotEmpty && pincode_controller.text.isNotEmpty
+            && business_description_controller.text.isNotEmpty
+                      ) {
+                         Business_Name = business_name_controller.text;
+            Owner_Name = owner_name_controller.text;
+            Phone_No = phone_no_controller.text;
+            Email = email_controller.text;
+            Address = address_controller.text;
+            Pincode = pincode_controller.text;
+            Business_Description = business_description_controller.text;
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                Identities(name_owner:Owner_Name, name_Business: Business_Name,
+      name_customername:[],
+      no_phoneno:[],
+      items_noofitems:[],
+      amount_totalamount:[],
+      bal_balance:[]),
+                          ));
+                      }
+                     
+                    },
+                    icon: Icon(Icons.send)),
+              ],
+            ),
+            
+          ],
         ),
       ),
     );
